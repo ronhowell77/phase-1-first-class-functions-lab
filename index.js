753d40344bac6d22ac1c7b1d01d72722fb84c183
Ronnie <ron.howell0326@gmail.com>
2022-10-01 00:13:27 -0400

    commit tree ef29bcc7cc5ed5afc349f43dd04ef97a9ac8d49d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2, 4);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(){
+    const fareMultiplier = function(){
+        return fareMultiplier.map(createFareMultiplier)
+    }
+}
